b438764ba51ad6460f69928959dba509afe2d47b
PhuQuy <quynvp95@gmail.com>
2022-11-04 19:26:56 +0700

    update

--- Login/js/loginfunction.js
+++
@@ -1,8 +0,0 @@
-
-var memberAccount = function (idHK, nameHK, passwordHK, emailHK) {
-    this.id = idHK;
-    this.nameMember = nameHK;
-    this.passwordMember = passwordHK,
-    this.emailMember = emailHK;
-}
-// }
